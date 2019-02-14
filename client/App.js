dc6c0d5cf0805f61ce8b9ae61ad634e37155c074
Grahn Cooledge <gcooled29@gmail.com>
2019-02-14 15:24:34 -0500

    Clean Up App.js File, Remove Unnecessary Code From File.

--- client/App.js
+++ client/App.js
@@ -1,68 +1,19 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
 
-import Users from './container/Users';
 import Event from './container/EventDetail';
 import asyncComponent from './hoc/asyncComponent';
-import Axios from 'axios';
 
 const AsyncPizza = asyncComponent(() => {
   return import('./container/Pizza.js');
 });
 
 class App extends Component {
-  state = {
-    guarantee: null,
-    event: null
-  };
-
-  // componentDidMount() {
-  //   this.fetchGuar();
-  //   this.fetchEvent();
-  // }
-
-  fetchGuar() {
-    Axios.get('/api/guarantee')
-      .then(({ data }) => {
-        console.log('TCL: App -> fetchGuar -> data', data);
-        this.setState({
-          guarantee: data.body
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  fetchEvent() {
-    Axios.get('/api/event/45')
-      .then(({ data }) => {
-        console.log('TCL: fetchEvent -> data', data);
-        this.setState({
-          event: data
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
   render() {
     return (
       <div>
         <div>
-          <Link
-            to={{
-              pathname: '/'
-              // state: {
-              //   guarantee: this.state.guarantee,
-              //   event: this.state.event
-              // }
-            }}
-          >
-            Event
-          </Link>{' '}
-          | <Link to="/pizza">Pizza</Link>
+          <Link to={'/'}>Event</Link> | <Link to="/pizza">Pizza</Link>
         </div>
         <div>
           <Route path="/" exact component={Event} />
