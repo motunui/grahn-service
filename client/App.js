9fb83a6870823d68296a58af384d02eaf47fb165
Grahn Cooledge <gcooled29@gmail.com>
2019-02-15 19:00:04 -0500

    Remove Erroneous Components, Files, and Containers form Project

--- client/App.js
+++ client/App.js
@@ -1,30 +1,20 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
 
+import classes from './App.css';
+import Product from './container/Product/Product';
+
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faShare, faBookmark } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faShare, faBookmark);
 
-import classes from './App.css';
-import asyncComponent from './hoc/asyncComponent';
-
-import Product from './container/Product/Product';
-
-const AsyncPizza = asyncComponent(() => {
-  return import('./container/Pizza.js');
-});
-
 class App extends Component {
   render() {
     return (
       <div className={classes.product_detail}>
-        <div>
-          <Link to={'/'}>Detail</Link> | <Link to="/pizza">Pizza</Link>
-        </div>
         <div className={classes.product_detail_header}>
-          <Route path="/" exact component={Product} />
-          <Route path="/pizza" component={AsyncPizza} />
+          <Product />
         </div>
       </div>
     );
