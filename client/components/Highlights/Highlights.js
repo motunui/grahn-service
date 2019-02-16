f873c0c011ebee58627122b35901bfc262c47735
Grahn Cooledge <gcooled29@gmail.com>
2019-02-15 20:21:35 -0500

    Fix Bullets On Highlights Components

--- client/components/Highlights/Highlights.js
+++ client/components/Highlights/Highlights.js
@@ -4,14 +4,16 @@ import classes from './Highlights.css';
 
 const Highlight = ({ highlights }) => (
   <div className={classes.overview_wrapper}>
-    <div className={classes.section_header}>Highlights</div>
-    <div className={classes.subsection}>
-      <ul>
-        {highlights.map((hl, i) => {
-          return <li key={i}>{hl.Text}</li>;
-        })}
-      </ul>
-      <div className={classes.show_more}>More</div>
+    <div className="section">
+      <div className={classes.section_header}>Highlights</div>
+      <div className={classes.subsection}>
+        <ul>
+          {highlights.map((hl, i) => {
+            return <li key={i}>{hl.Text}</li>;
+          })}
+        </ul>
+        <div className={classes.show_more}>More</div>
+      </div>
     </div>
   </div>
 );
