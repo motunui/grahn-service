f114d792c955ce4689bdae6c616fc6360043b783
Grahn Cooledge <gcooled29@gmail.com>
2019-02-21 22:19:17 -0500

    Rename App In Server To Server

--- server/server.js
+++ server/server.js
@@ -18,21 +18,22 @@ db.authenticate()
 
 config.sqlite.db = db;
 
-const app = express();
+const server = express();
 
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-// app.use(express.static(path.join(__dirname, '../build')));
+server.use(cors());
+server.use(morgan('dev'));
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static(path.join(__dirname, '../build')));
 
-app.use('/api', apiHandler(config));
+server.use('/api', apiHandler(config));
+
 // app.use('/:id', () => {
 //   console.log('In ID');
 //   express.static(path.join(__dirname, '../build'));
 // });
 
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Listening on Port : ${config.PORT}`);
 });
 
